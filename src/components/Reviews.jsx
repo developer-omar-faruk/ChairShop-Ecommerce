c98f591e2a2bb8d885d3f31c3192b27f5c0e0948
Developer-Omar-Faruk <mdomarfaruk1045@gmail.com>
2026-07-01 22:33:45 +0600

    Reviews Section

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -9,8 +9,8 @@ import reviewProfile3 from '../assets/reviewProfile3.png'
 
 const reviews = [
     {bg : review1, profile : reviewProfile1, name : "Bang Upin", title : "Pedagang Asongan" , dis : "“Terimakasih banyak, kini ruanganku menjadi lebih mewah dan terlihat mahal“"},
-    {bg : review1, profile : reviewProfile2, name : "Bang Upin", title : "Pedagang Asongan" , dis : "“Terimakasih banyak, kini ruanganku menjadi lebih mewah dan terlihat mahal“"},
-    {bg : review1, profile : reviewProfile3, name : "Bang Upin", title : "Pedagang Asongan" , dis : "“Terimakasih banyak, kini ruanganku menjadi lebih mewah dan terlihat mahal“"},
+    {bg : review2, profile : reviewProfile2, name : "Ibuk Sukijan", title : "Ibu Rumah Tangga", dis : "“Makasih Panto, aku sekarang berasa tinggal di apartment karena barang-barang yang terlihat mewah“"},
+    {bg : review3, profile : reviewProfile3, name : "Mpok Ina", title : "Karyawan Swasta" , dis : "“Sangat terjangkau untuk kantong saya yang tidak terlalu banyak“"}
 ];
 
 const ReviewCard = ({ bg, profile, name, title, dis }) => {
@@ -21,12 +21,12 @@ const ReviewCard = ({ bg, profile, name, title, dis }) => {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-      className="w-[370px] h-[476px] shadow-lg rounded-2xl shadow-black/50 items-end flex p-5"
+      className=" shadow-lg rounded-2xl w-fit shadow-black/50 items-end flex p-5"
     >
-        <div className="bg-white flex flex-col w-fit h-fit rounded-2xl relative gap-1 px-8 py-3 items-center">
+        <div className="bg-white mt-56  flex flex-col w-fit h-fit rounded-2xl relative gap-1 px-8 py-3 items-center">
             <img className="bg-white rounded-full absolute p-3 -top-8" src={profile} alt="" />
             <h1 className="font-semibold mt-8 text-xl">{name}</h1>
-            <h3 className="text-black/60">{title}</h3>
+            <h3 className="text-black/60 text-sm">{title}</h3>
             <p className="text-center mt-3 text-black/60">{dis}</p>
             <div className="flex gap-2 mt-4 text-lg text-amber-600">
                 <IoStarSharp />
@@ -42,8 +42,16 @@ const ReviewCard = ({ bg, profile, name, title, dis }) => {
 
 function Reviews() {
   return (
-    <div>
-      <ReviewCard bg={reviews[0].bg} profile={reviews[0].profile} name={reviews[0].name} title={reviews[0].title} dis={reviews[0].dis} />
+    <div className="overflow-hidden px-5 md:px-20">
+        <div className="mb-10">
+            <h3 className="text-amber-600 text-lg text-center uppercase">Testimonials</h3>
+            <h1 className=" className='text-4xl md:text-[42px] capitalize font-semibold text-center">Our Client Reviews</h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-15">
+            {reviews.map((d)=>(
+                <ReviewCard key={d.name} bg={d.bg} profile={d.profile} name={d.name} title={d.title} dis={d.dis}/>
+            ))}
+        </div>
     </div>
   );
 }
